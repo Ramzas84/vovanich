e289eac41769ce244bb96c2ef59ab67f7301b74f
Ramzas84 <88250395+Ramzas84@users.noreply.github.com>
2022-02-10 21:28:19 +0400

    Add files via upload

--- xox.cpp
+++ xox.cpp
@@ -1,94 +1,84 @@
 #include <iostream>
 
-
-
-int main (){
-    int sz = 3;
-    char desk[sz][sz] = {{'r', 't', 'z'},
-                        {'z', 'z', 'p'},
-                        {'z', 'z', 'r'}};
+void printArr (char** desk, int sz){
     for (int i = 0; i < sz; ++i){
-        for (int j = 0; j <sz; ++j){
+        for (int j = 0; j < sz; ++j){
             printf("%c ", desk[i][j]);
         }
         printf("\n");
     }
+}
 
-    bool win = false;
-    int i = 0;
-    char winner;
-
-    while (i < sz){
-        int temp = 1;
-        for (int j = 1; i <sz; ++j){
-            if (desk[i][j] == desk[i][j-1]){
-                ++temp;
+bool chechStr (char** arr, int sz, char &win){
+    for (int i = 0; i < sz; ++i){
+        for (int j = 1; j < sz; ++j){
+            if (arr[i][j] != arr[i][0]){break;}
+            if (j == sz - 1) {
+                win = arr[i][0];
+                return true;
             }
-            else {
-                break;
-            }
-        }
-        if ((temp == sz) && (desk[i][0] != ' ')){
-            win = true;
-            winner = desk[i][0];
-            printf("gorizontal! ");
-            break;
-        }
-        else ++i;
-    }
-
-    if (!win) {
-        i = 0;
-        while (i < sz){
-            int temp = 1;
-            for (int j = 1; i <sz; ++j){
-                if (desk[j][i] == desk[j - 1][i]){
-                    ++temp;
-                }
-                else {
-                    break;
-                }
-            }
-            if ((temp == sz) && (desk[0][i] != ' ')){
-                win = true;
-                winner = desk[0][i];
-                printf("vertikal! ");
-                break;
-            }
-            else ++i;
         }
     }
+    return false;
+}
 
-    if (!win) {
-        int temp = 1;
-        for (i = 1; i < sz; ++i){
-            if (desk[i][i] == desk[i - 1][i - 1]){
-                ++temp;
+bool checkColmn(char** arr, int sz, char &win){
+    if (chechStr(arr, sz, win)){return true;}
+    for (int i = 0; i < sz; ++i){
+        for (int j = 1; j < sz; ++j){
+            if (arr[j][i] != arr[0][i]){break;}
+            if (j == sz - 1) {
+                win = arr[0][i];
+                return true;
             }
         }
-        if (temp == sz){
-            win = true;
-            winner = desk[0][0];
-            printf("easy diagonal! ");
+    }
+    return false;
+}
+
+bool checkDiag(char** arr, int sz, char &win) {
+    if  (checkColmn(arr, sz, win)){return true;}
+    for (int i = 1; i < sz; ++i){
+        if (arr[i][i] != arr[0][0]) break;
+        if (i == sz - 1) {
+            win = arr[0][0];
+            return true;
         }
+    }
+    for (int i = 1; i < sz; ++i){
+        if (arr[i][sz - 1 - i] != arr[0][sz - 1]) break;
+        if (i == sz - 1) {
+            win = arr[0][sz - 1];
+            return true;
+        }
+    }
+    return false;
+}
+
+
+int main (){
+    int sz = 3;
+    char** desk;
+    desk = new char*[sz];
+    for (int i = 0; i < sz; ++i){
+        desk[i] = new char[sz];
     }
 
-    if (!win) {
-        int temp = 1;
-        for (i = 1; i < sz; ++i){
-            if (desk[i][sz - 1 - i] == desk[i - 1][sz - i]){
-                ++temp;
-            }
-        }
-        if (temp == sz){
-            win = true;
-            winner = desk[0][sz - 1];
-            printf("dif diagonal! ");
-        }
+    desk[0][0] = 'z';
+    desk[0][1] = 'x';
+    desk[0][2] = '3';
+    desk[1][0] = 'h';
+    desk[1][1] = '3';
+    desk[1][2] = 'x';
+    desk[2][0] = '3';
+    desk[2][1] = 'h';
+    desk[2][2] = 'z';
+
+    printArr (desk, sz);
+    char win;
+    if (checkDiag(desk, sz, win)){
+        printf("%c -- win!!", win);
     }
 
-    if (win) {
-        printf ("%c win!!", winner);
-    }
     return 0;
 }
